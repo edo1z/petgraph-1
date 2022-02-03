c70c08340ffc9b0a3dc5bec9746ca26a6e650328
web3ten0 <web3ten0@gmail.com>
2022-02-04 00:14:20 +0900

    added dfs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use petgraph::dot::{Config, Dot};
 use petgraph::graph::NodeIndex;
+use petgraph::prelude::Dfs;
 use petgraph::Graph;
 use std::fs::File;
 use std::io::prelude::*;
@@ -15,11 +16,44 @@ fn create_graph(nodes_len: usize, edges: &Vec<(usize, usize)>) -> Graph<i32, i32
     g
 }
 
-fn main() -> std::io::Result<()> {
-    let edges: Vec<(usize, usize)> = vec![(0, 1), (0, 2), (1, 2), (1, 4), (2, 3)];
-    let g = create_graph(5, &edges);
+fn create_dotfile(g: &Graph<i32, i32>, file_name: &str) -> std::io::Result<()> {
     let dot = Dot::with_config(&g, &[Config::EdgeNoLabel]);
-    let mut file = File::create("out.dot")?;
+    let mut file = File::create(file_name)?;
     file.write_all(dot.to_string().as_bytes())?;
+    Ok(())
+}
+
+fn dfs(g: &Graph<i32, i32>) -> Graph<i32, i32> {
+    let start: usize = 0;
+    let mut dfs = Dfs::new(&g, NodeIndex::new(start));
+    let mut route = vec![];
+    let mut prev = start;
+    let mut node_len = 0;
+    while let Some(node_idx) = dfs.next(&g) {
+        node_len += 1;
+        let idx = node_idx.index();
+        if idx != start {
+            route.push((prev, idx));
+            prev = node_idx.index();
+        }
+    }
+    create_graph(node_len, &route)
+}
+
+fn main() -> std::io::Result<()> {
+    let edges: Vec<(usize, usize)> = vec![
+        (0, 1),
+        (0, 2),
+        (1, 2),
+        (1, 4),
+        (2, 3),
+        (2, 7),
+        (0, 5),
+        (1, 6),
+    ];
+    let g = create_graph(8, &edges);
+    create_dotfile(&g, "graph.dot")?;
+    let g2 = dfs(&g);
+    create_dotfile(&g2, "dfs.dot")?;
     Ok(())
 }
